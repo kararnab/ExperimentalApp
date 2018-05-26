4e18e3cd41f91a0c5a54c217b66a65079aca14cf
Arnab Kar <arnabrocking@gmail.com>
2018-05-26 10:50:57 +0530

    Added moshi support

--- app/src/main/java/com/example/arnabkar/experiment1/MainActivity.java
+++ app/src/main/java/com/example/arnabkar/experiment1/MainActivity.java
@@ -3,13 +3,14 @@ package com.example.arnabkar.experiment1;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 import com.example.arnabkar.experiment1.model.Login;
 import com.example.arnabkar.experiment1.model.User;
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
 
-import java.io.IOError;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,14 +18,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
+
+        findViewById(R.id.imageView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String jsonToConvert = "{\"user\":{\"name\":\"Arnab\",\"address\":\"22 Rk das road\",\"emailId\":\"kararnab@github.com\",\"phoneNo\":98765443310,\"imgUrl\":null},\"msg\":\"ok\",\"success\":true,\"authKey\":\"awsfde32rwevsd422tgvgsdvsdvgvveasrs34_et34\"}";
+                try {
+                    Login login = writeJsonToClass(jsonToConvert, Login.class);
+                    Log.e("authKey is:",login.authKey);
+                }catch (IOException ignored){
+                    Log.e("Error",ignored.getMessage());
+                }
+            }
+        });
     }
 
-    void writeJsonToClass (String json) throws IOException{
+    <T> T writeJsonToClass(String json,Class<T> className) throws IOException{
         Moshi moshi = new Moshi.Builder().build();
-        JsonAdapter<Login> jsonAdapter = moshi.adapter(Login.class);
+        JsonAdapter<T> jsonAdapter = moshi.adapter(className);
 
-        Login blackjackHand = jsonAdapter.fromJson(json);
-        System.out.println(blackjackHand);
+        return jsonAdapter.fromJson(json);
     }
 
     void writeClassToJson(){
